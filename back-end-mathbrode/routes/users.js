77601d559113a6d2a9c530cc8ccd7f42f75a05a0
CamilleRungette <c.rungette@gmail.com>
2019-12-09 11:33:48 +0100

    Moved sign up route

--- back-end-mathbrode/routes/users.js
+++ back-end-mathbrode/routes/users.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+UserModel = require('../models/user')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+
+router.post('/sign-up', async function(req, res, next) {
+  console.log("================ SIGN UP FUNCTION ===============")
+
+  const userExists = await UserModel.findOne({ email: req.body.email})
+
+  if (userExists){
+    console.log("USER ALREADY EXISTS, CHANGE EMAIL")
+    res.json({userExists})
+  } else {
+   newUser = new UserModel({
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email: req.body.email,
+    password: req.body.password,  
+  })
+
+  newUser.save(function(error, user){
+    if (user){
+    console.log("NEW USER SAVED:", user)
+    res.json({user})
+    }else if (error){
+      console.log("USER NOR CREATED:", error)
+      res.json({error})
+    }
+  });
+}
+})
+
+
 
 module.exports = router;
