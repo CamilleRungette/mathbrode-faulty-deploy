317b27cef77b9a87fb80facc100fcea00642e90b
CamilleRungette <c.rungette@gmail.com>
2019-12-09 11:27:19 +0100

    user Schema created -- sign up function created

--- back-end-mathbrode/routes/index.js
+++ back-end-mathbrode/routes/index.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
+UserModel = require('../models/user')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+
 
 // // ADMIN 
 // router.post('/create-item', {
@@ -47,9 +48,33 @@ module.exports = router;
 
 // })
 
-// router.post('/sign-up', {
+router.post('/sign-up', async function(req, res, next) {
+  console.log("================ SIGN UP FUNCTION ===============")
 
-// })
+  const userExists = await UserModel.findOne({ email: req.body.email})
+
+  if (userExists){
+    console.log("USER ALREADY EXISTS, CHANGE EMAIL")
+    res.json({userExists})
+  } else {
+   newUser = new UserModel({
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email: req.body.email,
+    password: req.body.password,  
+  })
+
+  newUser.save(function(error, user){
+    if (user){
+    console.log("NEW USER SAVED:", user)
+    res.json({user})
+    }else if (error){
+      console.log("USER NOR CREATED:", error)
+      res.json({error})
+    }
+  });
+}
+})
 
 // router.get('/basket'{
 //   //Le panier utilisateur
@@ -90,3 +115,6 @@ module.exports = router;
 // router.get('/basket-buy', {
 //   //Passer commande depuis le panier
 // })
+
+
+module.exports = router;
