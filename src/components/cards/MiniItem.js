0a470e3a654b75306deb2e877c8aad11e8d66d7c
CamilleRungette <c.rungette@gmail.com>
2019-12-13 10:21:35 +0100

    dealt with the conflict

--- src/components/cards/MiniItem.js
+++ src/components/cards/MiniItem.js
@@ -2,39 +2,6 @@ import React from 'react';
 import {Link} from 'react-router-dom'
 
 
-class MiniItem extends React.Component {
-  
-
-
-  render() {
-
-  
-  
-return (
-
-
-  <div className="col-lg-4 my-3">
-      
-    <div className="productTitle">
-
-
-<Link to="/item" >
-  
-<div class="overlay-image">
- 
-  <a href="#">
- <img class="image" src="background 1.png" alt="Alt text" />
- <div class="normal">
-  <div class="text"></div>
- </div>
- <div class="hover">
-   <div class="zoom">
- <img class="imageBlurZoom" src="background 1.png" alt="" />
-  <div class="text">Nom<br/>12x35</div>
-  </div>
- </div>
-</a>
-</div>
 
 class MiniItem extends React.Component {
   render() {  
