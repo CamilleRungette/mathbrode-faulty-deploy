809af187e588385c7a1695678381b1115674bcaa
CamilleRungette <c.rungette@gmail.com>
2020-01-06 17:06:24 +0100

    ok

--- src/components/dashboardComponents/stock.js
+++ src/components/dashboardComponents/stock.js
@@ -107,9 +107,9 @@ class stock extends React.Component{
 }
 
     render(){
-    //   if (this.props.adminConnected == false || this.props.adminConnected == null){
-    //     return <Redirect to="/loginadmin" />
-    //  }
+      if (this.props.adminConnected == false || this.props.adminConnected == null){
+        return <Redirect to="/loginadmin" />
+     }
      console.log("checked", this.state.number)
       var show = unDisplayMessage
       if (this.state.show === true){
